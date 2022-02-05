6de2e8e70e88fdec9703b976bc36ca65ecc096f6
Jacob Darvin <jacob_darvin@dlsu.edu.ph>
2022-02-05 21:55:37 +0800

    Update Inputs.jsx

--- csarch-app/src/Inputs.jsx
+++ csarch-app/src/Inputs.jsx
@@ -2,8 +2,18 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Button, Typography } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 export default function Inputs() {
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
     return (
         <Box
             component="form"
@@ -13,12 +23,25 @@ export default function Inputs() {
             noValidate
             autoComplete="off"
         >
-            <Typography variant='h2'><b>The only <span className='specialColor'>Decimal-64</span> <br/>Converter You'll Ever Need.</b></Typography>
-            <Box sx={{ display: 'flex', mb: 1}}>
-                <TextField sx={{ flexGrow: 1, mr: 1 }} id="outlined-basic" type="number" label="Decimal" variant="outlined" />
-                <TextField sx={{ml: 1}} id="outlined-basic" type="number" label="Base-10" variant="outlined" />
+            <Typography variant='h2'><b>The only <span className='specialColor'>Decimal-64</span> <br />Converter You'll Ever Need.</b></Typography>
+            <Box sx={{ display: 'flex', mb: 1 }}>
+                <FormControl sx={{minWidth: 150, mr: 1}}>
+                    <InputLabel id="demo-simple-select-label">Method</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={age}
+                        label="Method"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={10}>Round Off</MenuItem>
+                        <MenuItem value={20}>NaN</MenuItem>
+                    </Select>
+                </FormControl>
+                <TextField sx={{ flexGrow: 5, mr: 1 }} id="outlined-basic" type="number" label="Decimal" variant="outlined" />
+                <TextField id="outlined-basic" type="number" label="Base-10" variant="outlined" />
             </Box>
-            <Button variant="contained" sx={{minWidth: '100%'}}><b>Convert</b></Button>
+            <Button variant="contained" sx={{ minWidth: '100%' }}><b>Convert</b></Button>
         </Box>
     );
 }
