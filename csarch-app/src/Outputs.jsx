071fd7bc13e495d96b1e35031be84ebb1e196b06
Chubbod <61444050+Chubbod@users.noreply.github.com>
2022-02-13 18:44:42 +0800

    Update Outputs.jsx
    Ties to even should 1st view the 16th even digit and if the 17th digit is 5 then this will happen.

--- csarch-app/src/Outputs.jsx
+++ csarch-app/src/Outputs.jsx
@@ -95,7 +95,7 @@ export default function Outputs(props) {
     
             } else if(props.method === 'Even') {
                 console.log("even")
-                if(parseInt(origInputString.charAt(16)) >= 6) { // +Inf
+                if(parseInt(origInputString.charAt(16)) % 2 === 0 && parseInt(origInputString.charAt(17)) >= 5) { // +Inf
                     origInputString = origInputString.substring(0, 16)
                     tempRestore = origInputString;
     
